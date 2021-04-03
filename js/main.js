dd758e619aec211da547cb42611a9a88e0a1eb61
Cherie <50756236+cybryant@users.noreply.github.com>
2021-04-02 20:04:23 -0400

    Added spatial and csv data to map
    converted spatial to geojson and added to map; joined to csv attribute data

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.onload = setMap();
 function setMap(){
     
     //set map dimensions
-    var width = 900, height = 500;    
+    var width = 1200, height = 600;    
     
     //create SVG container block
     var container = d3.select("body") //get the <body> element from the DOM
@@ -22,7 +22,7 @@ function setMap(){
         .center([-3.64, 38.15])
         .rotate([90.09, 0.00, 0])
         .parallels([34.05, 32.37])
-        .scale(75)
+        .scale(400)
         .translate([width / 2, height / 2]);
     
     var path = d3.geoPath()
@@ -31,7 +31,7 @@ function setMap(){
     //use Promise.all to parallelize asynchronous data loading
     var promises = [];
     promises.push(d3.csv("data/restaurant_cdc_data.csv")); //load attribute data
-    promises.push(d3.json("data/states_WGS84.topojson")); //load spatial data
+    promises.push(d3.json("data/states19.topojson")); //load spatial data
     Promise.all(promises).then(callback);
     
 	function callback(data){
@@ -43,8 +43,11 @@ function setMap(){
         console.log(restCDCdata);
         console.log(states);
         
+        //place graticule on the map
+        //PLACEHOLDER
+        
         //translate states TopoJson to GeoJson
-        var usStates = topojson.feature(states, states.objects.states_WGS84).features;
+        var usStates = topojson.feature(states, states.objects.states19).features;
         
         console.log(usStates);
         
@@ -57,6 +60,96 @@ function setMap(){
                 return d.properties.STUSPS;
             })
             .attr("d", path);
+        
+        //join csv data to GeoJSON enumeration units
+        usStates = joinData(usStates, restCDCdata);
+
+        //create the color scale
+        //var colorScale = makeColorScaleNatural(csvData);
+
+        //add enumeration units to the map
+        //setEnumerationUnits(franceRegions, map, path, colorScale);
+
+        //add coordinated visualization to the map
+        //setChart(csvData, colorScale);
     };
 
-};  //End setMap function    
+};  //End of setMap()    
+
+
+//join the attribute data to the spatial data
+function joinData(usStates, restCDCdata){
+    
+    //create list to hold attributes
+    var attrArray = ["fastFdPerMil", "veganPerMil", "vegetPerMil", "vegetOptPerMil", "totVegPerMil", "obesityPerc", "heartDisDthsPerMil"]; 
+    
+    //loop through csv data to assign each set of attribute values to geojson state
+    for (var i=0; i<restCDCdata.length; i++){
+        var csvState = restCDCdata[i]; //the current state
+        var csvKey = csvState.STUSPS; //the CSV primary key
+        
+        //loop through geojson state to find correct state
+        for (var a=0; a<usStates.length; a++){
+            var geojsonProps = usStates[a].properties; //the current state geojson properties
+            var geojsonKey = geojsonProps.STUSPS; //the geojson primary key
+            
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+                
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvState[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
+    console.log(usStates);
+    
+    return usStates;
+}; //end joinData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
